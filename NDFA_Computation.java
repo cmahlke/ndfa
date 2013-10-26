5899b487b692136bc24c0f59d38c1e5e65d8499f
Chris <cmm@post.harvard.edu>
2013-10-26 02:09:29 -0700

    Class representing one NDFA computation

---
+++ NDFA_Computation.java
@@ -0,0 +1,24 @@
+package Project;
+
+class NDFA_Computation {
+	NDFA nd;
+	String string;
+
+	public NDFA_Computation(NDFA n, String s) {
+	}
+
+	// returns the set of states we are in currently
+	public boolean[] get_current_set() {
+		return null;
+	}
+
+	// returns the rest of the string to compute on
+	public String get_string() {
+		return null;
+	}
+
+	// performs the next step in the computation
+	public int next_step() throws Rep_Exception {
+		return 0;
+	}
+}
